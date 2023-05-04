bfbbda9eb26da473995f01cbd4e77857f92f5493
Pipexlul <felipgg@gmail.com>
2023-05-04 16:03:40 -0400

    Add new command: invite-person. Send invitations to a person to join repositories that match your regex. Hardcoded limit of 10 repositories, to prevent spamming an user with invitation requests.

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,7 +1,8 @@
 import { Command } from "@commander-js/extra-typings";
 
 import addDiscWebhook from "./addDiscWebhook.js";
+import invitePerson from "./invitePerson.js";
 
-const commands: Command[] = [addDiscWebhook];
+const commands: Command[] = [addDiscWebhook, invitePerson];
 
 export default commands;
